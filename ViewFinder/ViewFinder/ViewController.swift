2fb0e1101423cf0378d7c9cf4cc4abd1f38403cb
Brandi Freeman <brandifreeman@Brandis-MacBook-Pro.local>
2019-05-18 08:57:55 -0500

    Initial interation

---
+++ ViewFinder/ViewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder
+//
+//  Created by Brandi Freeman on 5/18/19.
+//  Copyright © 2019 Brandi Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
